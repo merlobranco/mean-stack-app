59026c03343c3227ec8f9f9de6b5554f214dc455
Brais Cidras <braisomary@gmail.com>
2019-08-18 19:53:12 +0200

    Setting up Express to listen for requests

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("It works!");
+
+
+'use strict'
+
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), () => {
+	var port = server.address().port;
+	console.log('Magic happens on port ' + port);
+});
