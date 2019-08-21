e4bda0e570bdc732f123fe0fe0bad3d46959b422
Brais Cidras <braisomary@gmail.com>
2019-08-21 21:07:04 +0200

    Express - Exploring Middleware

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ var path = require('path');
 
 app.set('port', process.env.PORT || 3000);
 
+// Calling a custom middleware for all the requests
+// that will display all the called routes in the static route
+// It's working because is before the access to the static content
+// It will work only for the lines of code that are bellow
+// The order of the middleware is important it will run sequentially
+// one after the other
+app.use((req, res, next) => {
+	console.log(req.method, req.url);
+	next();
+});
+
 // Accesing to the static content of the public folder
-// In this case we are accesing to the home page
+// In this case we are accessing to the home page
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Sending a json object
-app.get('/json', function(req, res) {
+app.get('/json', (req, res) => {
 	console.log('GET the json');
 	res
 		.status(200)
@@ -19,7 +30,7 @@ app.get('/json', function(req, res) {
 });
 
 // Sending a file
-app.get('/file', function(req, res) {
+app.get('/file', (req, res) => {
 	console.log('GET the file');
 	res
 		.status(200)
