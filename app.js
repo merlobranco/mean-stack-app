73a6845e33b333c61afe8db94a79bdc7f91a5207
Brais Cidras <braisomary@gmail.com>
2019-08-21 19:36:23 +0200

    Express - Routing: defining URL routes and sending response

--- app.js
+++ app.js
@@ -4,7 +4,33 @@
 
 var express = require('express');
 var app = express();
+var path = require('path');
+
 app.set('port', process.env.PORT || 3000);
+
+// Sending Home Page
+app.get('/', function(req, res) {
+	console.log('GET the homepage');
+	res
+		.status(200)
+		.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Sending a json object
+app.get('/json', function(req, res) {
+	console.log('GET the json');
+	res
+		.status(200)
+		.json({'jsonData': true});
+});
+
+// Sending a file
+app.get('/file', function(req, res) {
+	console.log('GET the file');
+	res
+		.status(200)
+		.sendFile(path.join(__dirname, 'app.js'));
+});
 
 var server = app.listen(app.get('port'), () => {
 	var port = server.address().port;
