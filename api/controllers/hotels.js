1c0eaa9f3e91d2371892fabf1ed0f6022a1631cb
Brais Cidras <braisomary@gmail.com>
2019-09-30 10:43:56 +0200

    APIs - Finding documents based on geo-coordinates

--- api/controllers/hotels.js
+++ api/controllers/hotels.js
@@ -5,6 +5,31 @@
 var mongoose = require('mongoose');
 var Hotel = mongoose.model('Hotel');
 
+var runGeoQuery = function(req, res) {
+	var lng = parseFloat(req.query.lng);
+	var lat = parseFloat(req.query.lat);
+
+	Hotel
+		.aggregate([
+			{
+		    	$geoNear: {
+		        	near: { type: "Point", coordinates: [lng, lat] },
+		        	distanceField: "dist.calculated",
+		        	maxDistance: 2000, // Expressed in meters
+		        	// query: { 'currency': 'HUF' }, // Applying filters
+		        	// includeLocs: "dist.location",
+		        	spherical: true
+		    	}
+		   },
+	   		{ $limit: 5 } // Number of returned records
+	   	], (err, results) => {
+	   		console.log('Geo results: ', results);
+	   		res
+				.status(200)
+				.json(results);
+	   	});
+}
+
 
 var getAll = function(req, res) {
 
@@ -13,6 +38,11 @@ var getAll = function(req, res) {
 
 	var offset = 0;
 	var count = 5;
+
+	if (req.query && req.query.lat && req.query.lng) {
+		runGeoQuery(req, res);
+		return;
+	}
 
 	if (req.query && req.query.offset) {
 		offset = parseInt(req.query.offset, 10);
--- api/routes/index.js
+++ api/routes/index.js
@@ -10,6 +10,11 @@ var ReviewsController = require('../controllers/reviews');
 // Adding routes to the router with the path we want to map and te method we want to use
 // Sending a json object
 router.get('/hotels', HotelsController.getAll);
+// Shorcut for
+// router
+// 	.route('/hotels')
+// 	.get(HotelsController.getAll);
+
 
 router.get('/hotels/:id', HotelsController.getHotel);
 
