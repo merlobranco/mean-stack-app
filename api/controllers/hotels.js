a9f1923ab25a6d5194cfd54d5ceedc3064cb3745
Brais Cidras <braisomary@gmail.com>
2019-10-12 21:43:45 +0200

    APIs - Error trapping and hardening a REST APIs

--- api/controllers/hotels.js
+++ api/controllers/hotels.js
@@ -33,11 +33,9 @@ var runGeoQuery = function(req, res) {
 
 var getAll = function(req, res) {
 
-	// Retrieving the query string parameters
-	console.log(req.query);
-
 	var offset = 0;
 	var count = 5;
+	var maxCount = 10;
 
 	if (req.query && req.query.lat && req.query.lng) {
 		runGeoQuery(req, res);
@@ -52,13 +50,35 @@ var getAll = function(req, res) {
 		count = parseInt(req.query.count, 10);
 	}
 
+	if (isNaN(offset) || isNaN(count)) {
+		res
+			.status(400)
+			.json({'message': 'If supplied in querystring count and offset should be numbers'});
+		return;
+	}
+
+	if (count > maxCount) {
+		res
+			.status(400)
+			.json({'message': 'Count limit of ' + maxCount + ' exceeded'});
+		return;
+	}
+
 	Hotel
 		.find()
 		.skip(offset)
 		.limit(count)
 		.exec((err, hotels) => {
-			console.log('Found hotels: ', hotels.length);
-			res.json(hotels);
+			if (err) {
+				console.log('Error finding hotels');
+				res
+					.status(500)
+					.json(err);
+			} 
+			else {
+				console.log('Found hotels: ', hotels.length);
+				res.json(hotels); // By default the status is 200
+			}	
 	});
 };
 
@@ -69,11 +89,24 @@ var getHotel = function(req, res) {
 	Hotel
 		.findById(id)
 		.exec((err, data) => {
-			console.log('Found hotel', data);
+			var response = {
+				status: 200,
+				message: data
+			};
+			if (err) {
+				console.log('Error finding the hotel');
+				response.status = 500;
+				response.message = err;
+			} 
+			else if (!data) {
+				response.status = 404;
+				response.message = {'message': 'Hotel with ID: ' + id + 'not found'};
+			}
 			res
-				.status(200)
-				.json(data);
+				.status(response.status)
+				.json(response.message);
 			});
+				
 }
 
 var addHotel = function(req, res) {
