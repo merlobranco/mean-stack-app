5d46c40250ca20eeb4ded2b2a0fbb0cee4bce0c7
Brais Cidras <braisomary@gmail.com>
2019-10-28 19:45:44 +0200

    APIs - Creating new documents in MongoDB using Mongoose and POST methods

--- api/controllers/hotels.js
+++ api/controllers/hotels.js
@@ -122,26 +122,6 @@ var getHotel = function(req, res) {
 };
 
 var addHotel = function(req, res) {
-	// console.log("POST new hotel");
-
-	// if (!req.body || !req.body.name || !req.body.stars) {
-	// 	console.log('Data missing from body');
-	// 	res
-	// 		.status(400)
-	// 		.json({message: 'Required data missing from body'});
-	// }
-
-	// var hotel = req.body;
-	// hotel.stars = parseInt(hotel.stars, 10);
-	
-	// var db = dbConn.get();
-	// db.collection('hotels')
-	// 	.insertOne(hotel, (err, data) => {
-	// 		console.log(hotel);
-	// 		res
-	// 			.status(201)
-	// 			.json(data.ops);
-	// 	});
 	Hotel
 		.create({
 			name: req.body.name,
